e6962fd1432b64de6ab86bcd86089c378088806c
Douglas Smith <smithda@gmail.com>
2022-07-22 07:33:05 -0400

    Updating ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "plugin:storybook/recommended"]
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    commonjs: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+  },
+  plugins: ['react', '@next/eslint-plugin-next', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
