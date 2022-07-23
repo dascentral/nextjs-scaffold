e92c5d225a17a25960e88ed945b7a131b55f770c
Douglas Smith <smithda@gmail.com>
2022-07-23 08:38:06 -0400

    Adding @storybook/addon-postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,11 +7,19 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   staticDirs: ['../public'],
   framework: '@storybook/react',
   core: {
-  }
-}
     builder: '@storybook/builder-webpack5',
+  },
+};
